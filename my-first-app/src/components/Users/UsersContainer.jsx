b1f0ec6b9360f1b2891d7f8441ec2c22b9b83e84
DimaGenkai <immonnj6@gmail.com>
2023-05-08 23:00:45 +0400

    54-55 / 101 _ HOME

--- my-first-app/src/components/Users/UsersContainer.jsx
+++ my-first-app/src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
-import { followAC, unfollowAC, setUsersAC } from "../../redux/usersReducer"
+import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/usersReducer"
 import s from "./Users.module.css"
 import { connect } from "react-redux"
 import Users from "./Users"
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     } 
 }
 
@@ -19,6 +22,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (user) => {
             dispatch(setUsersAC(user))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
